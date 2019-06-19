4a94cf9598305c52028d41c8be60526fa663015a
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-19 02:15:51 -0700

    add search filter and regular filtering

--- profiles_api/profiles/api/urls.py
+++ profiles_api/profiles/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProfileViewSet, ProfileStatusViewSet
+from .views import ProfileViewSet, ProfileStatusViewSet, AvatarUpdateView
 
 router = DefaultRouter()
-router.register(r"profiles", ProfileViewSet)
-router.register(r"status", ProfileStatusViewSet)
+router.register(r"profiles", ProfileViewSet, base_name="profiles")
+router.register(r"status", ProfileStatusViewSet, base_name="status")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("avatar/", AvatarUpdateView.as_view(), name="api-avatar-update"),
+]
--- profiles_api/profiles/api/views.py
+++ profiles_api/profiles/api/views.py
@@ -1,23 +1,44 @@
-from rest_framework import generics
+from rest_framework.generics import UpdateAPIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import mixins
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsOwnerProfileOrReadOnly, IsOwnerOrReadOnly
 from ..models import Profile, ProfileStatus
-from .serializers import ProfileSerializer, ProfileStatusSerializer
+from .serializers import (
+    ProfileSerializer,
+    ProfileStatusSerializer,
+    ProfileAvatarSerializer,
+)
+
+
+class AvatarUpdateView(UpdateAPIView):
+    serializer_class = ProfileAvatarSerializer
+    permission_class = [IsAuthenticated]
+
+    def get_object(self):
+        profile_object = self.request.user.profile
+        return profile_object
 
 
 class ProfileViewSet(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_class = [IsOwnerProfileOrReadOnly, IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
 
 
 class ProfileStatusViewSet(ModelViewSet):
-    queryset = ProfileStatus.objects.all()
     serializer_class = ProfileStatusSerializer
     permission_class = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfileStatus.objects.all()
+        username = self.request.query_params.get("username", None)
+        if username:
+            queryset = queryset.filter(user_profile_user_username=username)
+        return queryset
 
     def perform_create(self, serializer):
         user_profile = self.request.user.profile
